ce8e231a2849905f70d7048762d5cc9e059b4444
mo-ro <gainmoro@gmail.com>
2018-10-25 22:47:39 -0700

    created works show page

--- src/components/heading.js
+++ src/components/heading.js
@@ -3,9 +3,9 @@ import React from 'react'
 class Heading extends React.Component {
   render() {
     return (
-      <div className="grobal-heading" data-text="true">
+      <h2 className="grobal-heading" data-type={this.props.type} data-text="true">
         {this.props.text}
-      </div>
+      </h2>
     )
   }
 }
--- src/pages/workTemplate.js
+++ src/pages/workTemplate.js
@@ -2,26 +2,48 @@ import React from 'react'
 import Layout from '../components/layout'
 import Heading from '../components/heading'
 
+import '../style/pages/workShow/index.scss'
 
 class workTemplate extends React.Component {
+
+  getTools(tools) {
+    let toolsText = ""
+    tools.forEach((tool, index) => {
+      toolsText += tool
+      if(index !== tools.length-1) {
+        toolsText += " / "
+      }
+    })
+    return toolsText
+  }
+
   render() {
     const data = this.props.pageContext.page;
     return (
       <Layout>
-        <div className="workShow-wrapper">
+        <div className="workshow-wrapper">
           <Heading text="Motion Work"/>
-          <main className="workShow-content">
+          <main className="workshow-content">
             <div className="left">
               {data.images.map((image, key) => {
                 const path = require(`../images/${image}`)
                 return (
-                  <img src={path} key={key} alt=""/>
+                  <img className="image" data-text="true" src={path} key={key} alt=""/>
                 )
               })}
             </div>
             <div className="right">
-              <div className="workShow-info">
-                <h1 className="name">{data.name}</h1>
+              <div className="workshow-info">
+                <h1 className="name" data-text="true">{data.name}</h1>
+                <section className="section">
+                  <Heading text="Description" type="sub"/>
+                  <p className="text" data-text="true">{data.description}</p>
+                </section>
+                <section className="section">
+                  <Heading text="Tools" type="sub"/>
+                  <p className="text" data-text="true">{this.getTools(data.tools)}</p>
+                </section>
+                <p className="date" data-text="true">{data.created}</p>
               </div>
             </div>
           </main>
