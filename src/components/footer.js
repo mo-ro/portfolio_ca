ef974b93cf58745bc2de399a659d33b2643bbca4
mo-ro <gainmoro@gmail.com>
2018-11-08 19:54:07 -0800

    changed to purecomponent

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 import moai from '../images/moai.svg'
 
-class Footer extends React.Component {
+class Footer extends React.PureComponent {
+
+  shouldComponentUpdate() {
+    return false
+  }
+  
   render() {
     return (
       <footer className="global-footer">
